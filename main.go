f90d324cdac42ab6fa3bedef1df19666af11c0b3
Mahdi Abdolmaleki <mahdi.cpp@gmail.com>
2022-01-10 15:07:28 +0330

    Update main.go

--- main.go
+++ main.go
@@ -8,16 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"instagram/config"
-	"instagram/routes"
-	"io/ioutil"
-	"log"
-	"math"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 
