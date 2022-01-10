d8bc4616926a53e00e24730c2007e33ba15d7da0
Mahdi Abdolmaleki <mahdi.cpp@gmail.com>
2022-01-10 13:04:07 +0330

    Create main.go

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"instagram/config"
+	"instagram/routes"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func main() {
+	bucket := "mahdicpp"
+	filename := "file.txt"
+
+	file, err := os.Open("/home/mahdi/file.txt")
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	defer file.Close()
+
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials("02b5f145-6b3e-468c-94d1-3e02bb834f0d", "630a75917ce5d93e5f7cb82b6b3e7d8d3e0d64645d49728ae46f308ec36d90b2", ""),
+		Region:      aws.String("default"),
+		Endpoint:    aws.String("https://s3.ir-thr-at1.arvanstorage.com"),
+	})
+
+	// Setup the S3 Upload Manager. Also see the SDK doc for the Upload Manager
+	// for more information on configuring part size, and concurrency.
+	//
+	// http://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/#NewUploader
+	uploader := s3manager.NewUploader(sess)
+
+	// Upload the file's body to S3 bucket as an object with the key being the
+	// same as the filename.
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+
+		// Can also use the `filepath` standard library package to modify the
+		// filename as need for an S3 object key. Such as turning absolute path
+		// to a relative path.
+		Key: aws.String(filename),
+
+		// The file to be uploaded. io.ReadSeeker is preferred as the Uploader
+		// will be able to optimize memory when uploading large content. io.Reader
+		// is supported, but will require buffering of the reader's bytes for
+		// each part.
+		Body: file,
+	})
+	if err != nil {
+		// Print the error and exit.
+		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
+	}
+
+	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+}
